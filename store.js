c69c9535c960af6a00a73912c2c104edbca2e318
Elle <elli.g.pope@gmail.com>
2017-05-14 18:37:36 -0700

    created a function to add the new divs to the div table. The div row has an extra class called data. Everytime the function is called, all divs with class data are deleted and regenerated. This will be usefule if user wants to sort later.

--- store.js
+++ store.js
@@ -5,14 +5,14 @@
 //-----------------------------------------------------------------------------//
 //These watch for button clicks to change the form
 
-$('#submit-data').on('click', getData);
+$('#add-data').on('click', getData);
 
 
 //-----------------------------------------------------------------------------//
 
 var chArr= [];
 
-function getData(stats){
+function getData(){
 	var chName = $('#data-name').val();
 	var chRace = $('#select-race option:selected').val();
 	var chClass = $('#select-class option:selected').val();
@@ -23,8 +23,39 @@ function getData(stats){
 	var chWis = $('#data-wis').val();
 	var chCha = $('#data-cha').val();
 	var chPor = $('#data-file').val();
+
+	var chart = $('#database-layout');
+	var row = $('<div>').addClass('row');
+  var col = $('<div>').addClass('col');
 	
-	chArr.push({name: chName, race: chRace, class: chClass, str: chStr, dex: chDex, con: chCon, int: chInt, wis: chWis, cha: chCha, portrait: chPor});
+	var newArrayObj = {name: chName, race: chRace, class: chClass, str: chStr, dex: chDex, con: chCon, int: chInt, wis: chWis, cha: chCha, portrait: chPor};
+
+  chArr.push(newArrayObj);
+	$('.data').remove();  //This deletes all chart rows and regenerates them... allows for sorting abilites in the array later
+	chArr.forEach(generateTable);
 	
-	event.preventdefault();
+	$('input').val('');
+	$('select').removeAttr('selected');
+	event.preventDefault();
+}
+
+function generateTable (chStats){
+  var chart = $('#database-layout');
+	var row = $('<div>').addClass('row data');
+	
+	var col = $('<div>').addClass('col').text(chStats.name);
+	row.append(col);
+	col = $('<div>').addClass('col').text(chStats.race);
+	row.append(col);
+	col = $('<div>').addClass('col').text(chStats.class);
+	row.append(col);
+	col = $('<div>').addClass('col').html('<p>Str:'+chStats.str+'</p>'+'<p>Dex:'+chStats.dex+'</p>'+'<p>Con:'+chStats.con+'</p>'+'<p>Int:'+chStats.int+'</p>'+'<p>Wis:'+chStats.wis+'</p>'+'<p>Cha:'+chStats.cha+'</p>');
+	row.append(col);
+	col = $('<div>').addClass('col').text(chStats.portrait);
+	row.append(col);
+	col=$('<div>').addClass('col').html('<button id="edit-data">Edit</button><button id="delete-data">delete</button>');
+	row.append(col);
+
+	chart.append(row);
+	event.preventDefault();
 }
